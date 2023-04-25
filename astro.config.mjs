88ae0c93c7e040bd9462e33aaf5154f89eb65c1b
peoray <peoray@yahoo.com>
2023-04-25 18:53:30 +0100

    chore: setup project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+import image from "@astrojs/image";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sitemap(), image(), mdx()]
+});
