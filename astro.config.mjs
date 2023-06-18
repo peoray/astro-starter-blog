a4214b80acb1433f5bb11d0c53fb8b985bb05389
peoray <peoray@yahoo.com>
2023-06-18 18:56:21 +0100

    chore: add site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,12 @@ import image from '@astrojs/image'
 import mdx from '@astrojs/mdx'
 import { remarkReadingTime } from './src/utils/remark-reading-time.mjs'
 
-// https://astro.build/config
 export default defineConfig({
-  // TODO: add site url
-  site: 'http://localhost:3000',
+  site: 'https://astro-starter-blog.netlify.app/',
   markdown: {
     syntaxHighlight: 'prism',
     remarkPlugins: [remarkReadingTime],
-    // gfm: true,
   },
-
   integrations: [sitemap(), image(), mdx()],
   experimental: {
     assets: true,
